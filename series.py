57579c8d3334bfaf2f3d7586d072946f5705979a
Amos Boldor <amosboldor@gmail.com>
2016-12-06 13:00:12 -0800

    lucas code written

--- series.py
+++ series.py
@@ -8,3 +8,12 @@ def fibonacci(n):
     elif n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def lucas(n):
+    """Return nth value of the lucas sequence."""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    return lucas(n - 1) + lucas(n - 2)

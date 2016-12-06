8835bc3d9d85cb4e64863e50d28082d7f9869c42
Ford Fowler <fordjfowler@gmail.com>
2016-12-06 14:30:08 -0800

    add demonstration of module functionality for terminal users

--- series.py
+++ series.py
@@ -1,4 +1,4 @@
-"""Define fibonacci and lucas functions."""
+"""Functions to calculate values of integer summation sequences."""
 
 
 def fibonacci(n):
@@ -20,9 +20,20 @@ def lucas(n):
 
 
 def sum_series(n, first=0, second=1):
-    """Return nth value (based on arguments)."""
+    """Return nth value of generalized summation sequence."""
     if n == 0:
         return first
     elif n == 1:
         return second
     return sum_series(n - 1, first, second) + sum_series(n - 2, first, second)
+
+
+if __name__ == "__main__":
+    print(__doc__, "\n...\n")
+    print("fibonacci(n):\n\n\t" + fibonacci.__doc__)
+    print("\n>>> fibonacci(3)\n2\n...\n")
+    print("lucas(n):\n\n\t" + lucas.__doc__)
+    print("\n>>> lucas(3)\n4\n...\n")
+    print("sum_series(n, [first=0], [second=1]):\n\n\t" + sum_series.__doc__)
+    print("\n>>> sum_series(3)\n2")
+    print("\n>>> sum_series(3, 2, 1)\n4")
